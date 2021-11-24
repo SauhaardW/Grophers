f388420b5345ec40fe5179db72dedebd7cc36d90
Nicole Droi <nicoled0210@hotmail.com>
2021-11-23 20:52:28 -0500

    Created owner product list page

---
+++ app/src/main/java/com/example/project/OwnerProductListActivity.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OwnerProductListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_product_list);
+    }
+}
