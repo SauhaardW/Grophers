17457810e629f588191421997f352b083c9e43b1
mojohosseinii <hosseini.sayedmojtaba@gmail.com>
2021-11-24 11:13:14 -0500

    Added Past Orders Customer Page
    Created the java file and xml page for Past Orders Customer. Created a template for past orders that can be changed according to what is required

---
+++ app/src/main/java/com/example/project/PastOrdersCustomer.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PastOrdersCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_orders_customer);
+    }
+}
