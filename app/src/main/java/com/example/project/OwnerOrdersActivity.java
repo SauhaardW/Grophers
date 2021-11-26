945180bbc2ac3737c719cebb5599752a33b0e036
Nicole Droi <nicoled0210@hotmail.com>
2021-11-25 23:50:43 -0500

    Recreated the order info modal and implemented the recycler view in it imdediatly

--- app/src/main/java/com/example/project/OwnerOrdersActivity.java
+++ app/src/main/java/com/example/project/OwnerOrdersActivity.java
@@ -3,6 +3,8 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class OwnerOrdersActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class OwnerOrdersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_orders);
-    }
+
+        TextView orderInfo = (TextView) findViewById(R.id.orderInfoButton);
+        orderInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OrderInfoModalDialog modalDialog = new OrderInfoModalDialog();
+                modalDialog.show(getSupportFragmentManager(), "ownerOrderInfoModal");
+            }//end onClick
+        });
+    }//end onCreate
 }
