ca40558e562cde05cf17042e9f2c861385572e96
Nicole Droi <nicoled0210@hotmail.com>
2021-11-25 21:04:09 -0500

    Created modal to Add products

---
+++ app/src/main/java/com/example/project/AddProductModalDialog.java
@@ -0,0 +1,21 @@
+package com.example.project;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class AddProductModalDialog extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.add_product_modal, container, false);
+        return v;
+    }//end onCreateView
+}//end AddProductModalDialog
--- app/src/main/java/com/example/project/CustomerStoreProductViewActivity.java
+++ app/src/main/java/com/example/project/CustomerStoreProductViewActivity.java
@@ -3,6 +3,8 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CustomerStoreProductViewActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class CustomerStoreProductViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_store_product_view);
+
+        ImageView productBackgroundImg = (ImageView) findViewById(R.id.productsBackgroundCustomreStoreProductView);
+        productBackgroundImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AddProductModalDialog addProductModalDialog = new AddProductModalDialog();
+                addProductModalDialog.show(getSupportFragmentManager(), "addProductModal");
+            }
+        });
     }
 }
