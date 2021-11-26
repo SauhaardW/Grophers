735c04cd93cd7930d394ec98c04dd95da6002f24
Nicole Droi <nicoled0210@hotmail.com>
2021-11-26 12:29:35 -0500

    Finished the spinner within the modal

--- app/src/main/java/com/example/project/OwnerOrdersActivity.java
+++ app/src/main/java/com/example/project/OwnerOrdersActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class OwnerOrdersActivity extends AppCompatActivity {
 
@@ -16,12 +21,21 @@ public class OwnerOrdersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_owner_orders);
 
         TextView orderInfo = (TextView) findViewById(R.id.orderInfoButton);
-        orderInfo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                OrderInfoModalDialog modalDialog = new OrderInfoModalDialog();
-                modalDialog.show(getSupportFragmentManager(), "ownerOrderInfoModal");
-            }//end onClick
+        orderInfo.setOnClickListener(view -> {
+            BottomSheetDialog orderInfoModal = new BottomSheetDialog(OwnerOrdersActivity.this);
+            View modalView = getLayoutInflater().inflate(R.layout.order_info_modal, null);
+
+            createSpinner(modalView);
+
+            orderInfoModal.setContentView(modalView);
+            orderInfoModal.show();
         });
     }//end onCreate
+
+    public void createSpinner(View modalView){
+        Spinner modalSpinner = (Spinner) modalView.findViewById(R.id.spinnerChoicesOrderInfo);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(OwnerOrdersActivity.this, android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.orderInfoModalList));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        modalSpinner.setAdapter(adapter);
+    }//end createSpinner
 }
