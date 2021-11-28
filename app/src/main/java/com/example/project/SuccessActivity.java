ba11b495e5ba4a681a891ca70526a58802b2ba70
Andres <andrewpriority1@gmail.com>
2021-11-27 22:43:05 -0500

    Implemented order placing and cart display page

--- app/src/main/java/com/example/project/SuccessActivity.java
+++ app/src/main/java/com/example/project/SuccessActivity.java
@@ -2,7 +2,9 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SuccessActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class SuccessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SuccessActivity.this, CustomerViewActivity.class));
+                finish();
+            }
+        }, 5000);
     }
 }
