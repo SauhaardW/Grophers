1750fca4f1883b3240301c82f9b4edd5c963ef45
mojohosseinii <hosseini.sayedmojtaba@gmail.com>
2021-12-01 02:23:14 -0500

    Owner Products backend

--- app/src/main/java/com/example/project/OwnerProductListActivity.java
+++ app/src/main/java/com/example/project/OwnerProductListActivity.java
@@ -1,14 +1,93 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class OwnerProductListActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    DatabaseReference db;
+    OwnerProductListViewAdapter adapter;
+    ArrayList<Product> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_product_list);
+
+
+
+
+        recyclerView = findViewById(R.id.recyclerViewProductOwner);
+        db = FirebaseDatabase.getInstance().getReference("stores");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+
+
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        FirebaseDatabase.getInstance().getReference("users").child("owners").child(uid).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()) {
+                    Integer store_id = task.getResult().child("storeId").getValue(int.class);
+                    String store_name = task.getResult().child("name").getValue(String.class);
+
+                    adapter = new OwnerProductListViewAdapter(OwnerProductListActivity.this, list, store_id.toString());
+                    recyclerView.setAdapter(adapter);
+
+                    Button createProductButton = findViewById(R.id.createProductButtonInvisible);
+                    createProductButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(OwnerProductListActivity.this, AddProductModalDialog.class);
+                            intent.putExtra("store_id", store_id);
+                            intent.putExtra("store_name", store_name);
+                            startActivity(intent);
+                        }
+                    });
+                    db.child(store_id.toString()).child("products").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DataSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (DataSnapshot dataSnapshot : task.getResult().getChildren()) {
+                                    Product product = dataSnapshot.getValue(Product.class);
+                                    list.add(product);
+                                }
+                                adapter.notifyDataSetChanged();
+                            } else {
+                                Toast.makeText(OwnerProductListActivity.this, "Error while getting store data", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(OwnerProductListActivity.this, "Error while getting user data", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/project/OwnerProductListViewAdapter.java
@@ -0,0 +1,80 @@
+package com.example.project;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class OwnerProductListViewAdapter extends RecyclerView.Adapter<OwnerProductListViewAdapter.ViewHolder>{
+    Context context;
+    ArrayList<Product> list;
+    String storeId;
+
+    public OwnerProductListViewAdapter(Context context, ArrayList<Product> list, String storeId) {
+        this.context = context;
+        this.list = list;
+        this.storeId = storeId;
+    }
+
+    @NonNull
+    @Override
+    public OwnerProductListViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.card_owner_product, parent, false);
+        return new OwnerProductListViewAdapter.ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OwnerProductListViewAdapter.ViewHolder holder, int position) {
+        Product product = list.get(position);
+        holder.productName.setText(product.getBrand() + " " + product.getName());
+        holder.productPrice.setText("$" + String.format("%.2f", product.getPrice()));
+        //implement image setting
+
+        CardView card = (CardView) holder.itemView.findViewById(R.id.productCardOwner);
+        card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditProductModalDialog editProductModalDialog = new EditProductModalDialog();
+                Bundle bundle = new Bundle();
+                bundle.putString("product_name", product.getName());
+                bundle.putString("product_brand", product.getBrand());
+                bundle.putString("product_price", "$" + String.format("%.2f", product.getPrice()));
+                bundle.putDouble("product_price_double", product.getPrice());
+                bundle.putString("product_img", product.getImage());
+                bundle.putString("product_id", ((Integer)product.getId()).toString());
+                bundle.putString("store_id", storeId);
+                editProductModalDialog.setArguments(bundle);
+                editProductModalDialog.show(((AppCompatActivity)context).getSupportFragmentManager(), "addProductModal");
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView productName, productPrice;
+        ImageView productImg;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.productName = itemView.findViewById(R.id.productNameText2);
+            this.productPrice = itemView.findViewById(R.id.textViewProductPrice2);
+            this.productImg = itemView.findViewById(R.id.imageViewproductCardOwner);
+        }
+    }
+}
