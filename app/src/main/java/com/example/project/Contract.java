eaca98598b801a28aeb06fc73251af88be82b514
Alejandro Iglesias Llobet <alejandro.iglesiasllobet3@gmail.com>
2021-12-05 14:21:41 -0500

    Creating Contract Interface

---
+++ app/src/main/java/com/example/project/Contract.java
@@ -0,0 +1,15 @@
+package com.example.project;
+
+public interface Contract {
+    public interface Model {
+        //
+    }
+
+    public interface View {
+        //
+    }
+
+    public interface Presenter {
+
+    }
+}
